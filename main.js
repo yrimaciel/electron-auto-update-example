188d688ef04d33b10911fdda5a468705500d6855
Yuri Maciel <54074819+yrimaciel@users.noreply.github.com>
2021-06-15 17:05:40 -0300

    update

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
 
 let mainWindow;
+
+autoUpdater.autoDownload = true
 
 function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    show: false,
     webPreferences: {
       nodeIntegration: true,
     },
@@ -15,13 +19,16 @@ function createWindow () {
   mainWindow.on('closed', function () {
     mainWindow = null;
   });
-  mainWindow.once('ready-to-show', () => {
-    autoUpdater.checkForUpdatesAndNotify();
-  });
 }
+
+setInterval(() => {
+  autoUpdater.checkForUpdates()
+}, 10000);
 
 app.on('ready', () => {
   createWindow();
+  mainWindow.show()
+  mainWindow.webContents.openDevTools()
 });
 
 app.on('window-all-closed', function () {
@@ -41,13 +48,37 @@ ipcMain.on('app_version', (event) => {
 });
 
 autoUpdater.on('update-available', () => {
+  log.info("update available")
+  console.log('update available')
+  window.alert("update available")
   mainWindow.webContents.send('update_available');
 });
 
 autoUpdater.on('update-downloaded', () => {
+  log.info("update downloaded")
+  console.log('update downloaded')
   mainWindow.webContents.send('update_downloaded');
 });
 
+autoUpdater.on('download-progress', (obj) => {
+  log.info(`Velocidade (mbps): ${obj.bytesPerSecond}`)
+  log.info(`Downloaded: ${obj.percent}%`)
+})
+
+ipcMain.on("download_app", () => {
+  log.info("download app")
+  try {
+    autoUpdater.downloadUpdate()
+  } catch (error) {
+    log.error(error)
+  }
+})
+
 ipcMain.on('restart_app', () => {
-  autoUpdater.quitAndInstall();
+  log.info("restart app")
+  try {
+    autoUpdater.quitAndInstall();
+  } catch (error) {
+    log.error(error)
+  }
 });
